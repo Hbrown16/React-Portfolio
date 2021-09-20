8651f2336c2e6d93d065006f704be1d17b43b2f5
Hbrown16 <hbrown1094@gmail.com>
2021-09-20 14:31:45 -0400

    nav bar

--- react-portfolio-haley/src/components/Nav/NavBar.js
+++ react-portfolio-haley/src/components/Nav/NavBar.js
@@ -1,7 +1,25 @@
 const NavBar = () => {
     return (
         <div>
-            <h1>Navigation bar</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/#about">About Me</a>
+                    </li>
+                    <li>
+                        <a href="/#skills">My Work</a>
+                    </li>
+                    <li>
+                        <a href="/#youtube">Videos</a>
+                    </li>
+                    <li>
+                        <a href="/#contact">Contact Me</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 }
