702c078b809b170697bf68c5eae06fa1f01bcf3f
Hbrown16 <hbrown1094@gmail.com>
2021-09-20 14:23:43 -0400

    importing files

--- react-portfolio-haley/src/App.js
+++ react-portfolio-haley/src/App.js
@@ -1,5 +1,9 @@
 import NavBar from './components/Nav/NavBar'
 import About from './components/About/aboutme'
+import Home from './components/Home/home'
+import Skills from './components/Skills/Skills'
+import Youtube from './components/Youtube/youtube'
+import Contact from './components/Contact/Contact'
 const App = () => {
   return (
     <div className="App">
