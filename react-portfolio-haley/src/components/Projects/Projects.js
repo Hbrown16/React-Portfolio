b21225e24c8e8b363d176d7c3abd5492ad264521
Hbrown16 <hbrown1094@gmail.com>
2021-09-21 01:28:17 -0400

    projects btn con

--- react-portfolio-haley/src/components/Projects/Projects.js
+++ react-portfolio-haley/src/components/Projects/Projects.js
@@ -4,7 +4,7 @@ import PageHeader from '../PageHeader/PageHeader'
 const linkBtnSvg = (url) => {
     return (
         <div className={StyleSheet.linkButton}
-        onClick={(e) => window.open(url, "_blank") focus}
+        onClick={(e) => window.open(url, "_blank").focus}
         >
          <svg class="svg-icon" viewBox="0 0 20 20">
              <path 
@@ -13,10 +13,11 @@ const linkBtnSvg = (url) => {
 								c-4.361,0-7.896,3.535-7.896,7.896s3.535,7.896,7.896,7.896c1.934,0,3.705-0.698,5.078-1.853l4.52,4.519
 								c0.266,0.268,0.699,0.268,0.965,0C19.396,18.863,19.396,18.431,19.129,18.164z M8.567,15.028c-3.568,0-6.461-2.893-6.461-6.461
 								s2.893-6.461,6.461-6.461c3.568,0,6.46,2.893,6.46,6.461S12.135,15.028,8.567,15.028z"
-        ></path>
+            ></path>
          </svg>
-    )
-}
+    </div>
+   );
+};
 
 const Youtube = () => {
     return (
