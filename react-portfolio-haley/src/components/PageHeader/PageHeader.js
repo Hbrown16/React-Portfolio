a2412cf5489695557d7497ca9ef5918e5d0940f8
Hbrown16 <hbrown1094@gmail.com>
2021-09-20 22:54:34 -0400

    page header

---
+++ react-portfolio-haley/src/components/PageHeader/PageHeader.js
@@ -0,0 +1,12 @@
+
+const PageHeader = () => {
+    return (
+        <div className={classes.Container}>
+            <hr />
+            <h1>{PaymentResponse.title}</h1>
+        </div>
+    );
+}
+ 
+
+export default PageHeader;
