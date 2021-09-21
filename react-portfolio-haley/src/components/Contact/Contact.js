89e4892a1d7dffb989719ecdeb9c33afa458c6d1
Hbrown16 <hbrown1094@gmail.com>
2021-09-21 03:02:02 -0400

    contact

--- react-portfolio-haley/src/components/Contact/Contact.js
+++ react-portfolio-haley/src/components/Contact/Contact.js
@@ -1,9 +1,22 @@
-import classes from '../Contact/Contact.module.js'
+import {FaLinkedin, FaGithub, FaMailBulk, FaPhoneAlt  } from "react-icons/fa";
+
+const handleURL = (url) => {
+    return even => window.open(url, "_blank")
+}
 
 const Contact = () => {
     return (
-        <div className={classes}>
-            <h1>Contact Me</h1>
+        <div className={classes.Contact}>
+            <div className={classes.ContactIcons}>
+            <FaLinkedin color= 'white' size= '30px' style={{padding: '1%'}}
+            onClick={handleURL('https://www.linkedin.com/in/haley-brown-b64a6727/')} />
+            <FaGithub color= 'white' size= '30px' style={{padding: '1%'}}
+            onClick={handleURL('https://github.com/Hbrown16')} />
+            <FaPhoneAlt color= 'white' size= '30px' style={{padding: '1%'}}
+            onClick={handleURL('https://hihello.me/p/cd3a643f-eb5e-4e32-b8f3-6aa060130695')} />
+            <FaMailBulk color= 'white' size= '30px' style={{padding: '1%'}}
+            onClick={handleURL('mailto:hbrown1094@gmail.com')} />
+        </div>
         </div>
     );
 }
