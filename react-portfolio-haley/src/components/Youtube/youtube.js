fb1fc394f6654e37ca225bf7f88bd6148e46729f
Hbrown16 <hbrown1094@gmail.com>
2021-09-21 01:15:31 -0400

    projects p

--- react-portfolio-haley/src/components/Youtube/youtube.js
+++ react-portfolio-haley/src/components/Youtube/youtube.js
@@ -7,7 +7,9 @@ const Youtube = () => {
             <div className={classes.ProjectsContent}>
                 <div className={classes.Paragraph}>
                     <p>
-                        
+                        Click on the images below to view each Github repository for the follwoing projects. There you will find a
+                        README.md file in each repo with a deployed link. Feel free to follow me on Github and reach out if you have 
+                        any questions.
                     </p>
                 </div>
             </div>
