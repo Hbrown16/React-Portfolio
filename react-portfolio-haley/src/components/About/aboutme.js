8736f0b8460fad10eb2d13543171bad4651e1ead
Hbrown16 <hbrown1094@gmail.com>
2021-09-20 23:19:27 -0400

    about me pic and description

--- react-portfolio-haley/src/components/About/aboutme.js
+++ react-portfolio-haley/src/components/About/aboutme.js
@@ -1,5 +1,6 @@
 import classes from '../About/About.module.css'
 import PageHeader from '../PageHeader/PageHeader'
+import me from './../../img/about-me.jpeg'
 const About = () => {
     return (
         <div className={classes.AboutMe}>
@@ -8,8 +9,16 @@ const About = () => {
                 <div className={classes.Text}>
                     <h2>Hello! I'm Haley</h2>
                     <p>
-                        
+                        Hello and thank you for viewing my portfolio. My name is Haley Brown and I am from charlotte
+                        but I currently live in Rock Hill, SC. I am working on becoming a Full Stack Web developer at
+                        the part time Full Stack Coding Bootcamp through the University of North Carolina at Charlotte.
+                        While I am getting my certification in web developement, I am working full time at Dick's Sporting Goods
+                        as a department lead. I have a background in customer service and I bring a unique perspective to programming. 
+                        I am definetly looking for a career change and I can not wait to join your team. 
                     </p>
+                </div>
+                <div className={classes.Photo}>
+                    <img className={classes.Me} src={me} alt="me"></img>
                 </div>
             </div>
         </div>
