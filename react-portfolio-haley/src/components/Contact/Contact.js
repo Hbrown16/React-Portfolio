c659f5d12a91ca4c46e1ecb6cdd4dcba35516347
Hbrown16 <hbrown1094@gmail.com>
2021-09-21 12:02:14 -0400

    styling

--- react-portfolio-haley/src/components/Contact/Contact.js
+++ react-portfolio-haley/src/components/Contact/Contact.js
@@ -9,13 +9,13 @@ const Contact = () => {
     return (
         <div className={classes.Contact} id ="contact">
             <div className={classes.ContactIcons}>
-            <FaLinkedin color= 'white' size= '30px' style={{padding: '1%'}}
+            <FaLinkedin color= 'white' size= '50px' style={{padding: '1%'}}
             onClick={handleURL('https://www.linkedin.com/in/haley-brown-b64a6727/')} />
-            <FaGithub color= 'white' size= '30px' style={{padding: '1%'}}
+            <FaGithub color= 'white' size= '50px' style={{padding: '1%'}}
             onClick={handleURL('https://github.com/Hbrown16')} />
-            <FaPhoneAlt color= 'white' size= '30px' style={{padding: '1%'}}
+            <FaPhoneAlt color= 'white' size= '50px' style={{padding: '1%'}}
             onClick={handleURL('https://hihello.me/p/cd3a643f-eb5e-4e32-b8f3-6aa060130695')} />
-            <FaMailBulk color= 'white' size= '30px' style={{padding: '1%'}}
+            <FaMailBulk color= 'white' size= '50px' style={{padding: '1%'}}
             onClick={handleURL('mailto:hbrown1094@gmail.com')} />
         </div>
         </div>
--- react-portfolio-haley/src/components/Home/home.js
+++ react-portfolio-haley/src/components/Home/home.js
@@ -1,15 +1,15 @@
 import classes from './Home.module.css'
-import code from './../../img/code.svg'
-import binaryWorld from './../../img/world.svg'
+// import code from './../../img/code.svg'
+// import binaryWorld from './../../img/world.svg'
 const Home = () => {
     return (
         <div className={classes.Home}>
             <div className={classes.Container}>
                 <h1 className={classes.Hello}>Hello</h1>
-                <h1>Welcome to Portfolio</h1>
+                <h1>Welcome to my Portfolio</h1>
             </div>
-            <img className={classes.Code} src={code} alt= "code"></img>
-            <img className={classes.BinaryWorld} src={binaryWorld} alt= "me"></img>
+            {/* <img className={classes.Code} src={code} alt= "code"></img> */}
+            {/* <img className={classes.BinaryWorld} src={binaryWorld} alt= "me"></img> */}
         </div>
     );
 }
