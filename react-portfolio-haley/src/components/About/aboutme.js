ae74f2f9619ed588a644cdd192877c3fca51ac27
Hbrown16 <hbrown1094@gmail.com>
2021-09-20 23:10:55 -0400

    about me

--- react-portfolio-haley/src/components/About/aboutme.js
+++ react-portfolio-haley/src/components/About/aboutme.js
@@ -2,8 +2,16 @@ import classes from '../About/About.module.css'
 import PageHeader from '../PageHeader/PageHeader'
 const About = () => {
     return (
-        <div>
+        <div className={classes.AboutMe}>
             <PageHeader title={'About Me'}/>
+            <div className={classes.Container}>
+                <div className={classes.Text}>
+                    <h2>Hello! I'm Haley</h2>
+                    <p>
+                        
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
