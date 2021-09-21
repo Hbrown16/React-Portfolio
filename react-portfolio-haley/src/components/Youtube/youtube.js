7d10fe81ec548ad406103976f6c65dcb59774b89
Hbrown16 <hbrown1094@gmail.com>
2021-09-21 01:12:51 -0400

    projects started

--- react-portfolio-haley/src/components/Youtube/youtube.js
+++ react-portfolio-haley/src/components/Youtube/youtube.js
@@ -1,7 +1,16 @@
+import classes from '../Youtube/Youtube.module.css'
+import PageHeader from '../PageHeader/PageHeader'
 const Youtube = () => {
     return (
-        <div>
-            <h1>Videos</h1>
+        <div className={classes.Projects}>
+            <PageHeader title={"Projects"} />
+            <div className={classes.ProjectsContent}>
+                <div className={classes.Paragraph}>
+                    <p>
+                        
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
