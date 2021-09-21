8c1c8cbd5cbf032b748b08152e4ae8a8bddd25c7
Hbrown16 <hbrown1094@gmail.com>
2021-09-20 21:57:23 -0400

    home

--- react-portfolio-haley/src/components/Home/home.js
+++ react-portfolio-haley/src/components/Home/home.js
@@ -1,7 +1,15 @@
+import classes from './Home/Home.module.css'
+import code from './../../img/background1.jpg'
+import binaryWorld from './../../img/about-me.jpeg'
 const Home = () => {
     return (
-        <div>
-            <h1>Haley Brown</h1>
+        <div className={classes.Home}>
+            <div className={classes.Container}>
+                <h1 className={classes.Hello}>Haley Brown</h1>
+                <h1>Welcome to Portfolio</h1>
+            </div>
+            <img className={classes.Code} src={code} alt= "code"></img>
+            <img className={classes.BinaryWorld} src={binaryWorld} alt= "me"></img>
         </div>
     );
 }
